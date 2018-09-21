b2b3207291dd7f7fb08b87cececaa909f84ce0c7
cbpark <cbpark@gmail.com>
2018-09-22 00:14:29 +0900

    Make random generator static

--- src/utils.cc
+++ src/utils.cc
@@ -11,9 +11,9 @@
 
 namespace mcggh {
 double getRandom() {
-    std::random_device rd;
-    std::mt19937_64 gen{rd()};  // mersenne_twistor_engine
-    std::uniform_real_distribution<double> dr{0.0, 1.0};
+    static std::random_device rd;
+    static std::mt19937_64 gen{rd()};  // mersenne_twistor_engine
+    static std::uniform_real_distribution<double> dr{0.0, 1.0};
     return dr(gen);
 }
 }  // namespace mcggh
